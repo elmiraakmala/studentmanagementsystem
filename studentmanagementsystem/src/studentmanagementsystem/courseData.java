3cbe8889513e33c2c213a27b115fc6b5e24934dc
elmiraakmala <elmiraakmala920@gmail.com>
2023-06-03 17:43:10 +0700

    tugas pbo - Netbeans javafx

---
+++ studentmanagementsystem/src/studentmanagementsystem/courseData.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StudentManagementSystem;
+
+
+public class courseData {
+    
+    private String course;
+    private String description;
+    private String degree;
+    
+    public courseData(String course, String description, String degree){
+        this.course = course;
+        this.description = description;
+        this.degree = degree;
+    }
+    public String getCourse(){
+        return course;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getDegree(){
+        return degree;
+    }
+    
+}
